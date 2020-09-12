e04f6dc76adc9510c3e29b4c0e823ce885114d25
Ned Batchelder <ned@nedbatchelder.com>
2020-09-12 10:14:10 -0400

    Simpler app

--- app.py
+++ app.py
@@ -46,83 +46,78 @@ SCOPE = (
     tk.scope.user_read_playback_state
 )
 
-def app_factory() -> Flask:
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'aliens'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'aliens'
 
-    @app.route('/', methods=['GET'])
-    def main():
-        user = session.get('user', None)
-        if user is not None:
-            token = users.get(user, None)
-        else:
-            token = None
+@app.route('/', methods=['GET'])
+def main():
+    user = session.get('user', None)
+    if user is not None:
+        token = users.get(user, None)
+    else:
+        token = None
 
-        # Return early if no login or old session
-        if user is None or token is None:
-            session.pop('user', None)
-            return 'You can <a href="/login">login</a>'
+    # Return early if no login or old session
+    if user is None or token is None:
+        session.pop('user', None)
+        return 'You can <a href="/login">login</a>'
 
-        if token.is_expiring:
-            token = cred.refresh(token)
-            users[user] = token
+    if token.is_expiring:
+        token = cred.refresh(token)
+        users[user] = token
 
-        spotify = tk.Spotify(token)
-        user = spotify.current_user()
-        page = f"User: {user.display_name}. "
-        page += f'<br>You can <a href="/logout">logout</a>'
+    spotify = tk.Spotify(token)
+    user = spotify.current_user()
+    page = f"User: {user.display_name}. "
+    page += f'<br>You can <a href="/logout">logout</a>'
 
-        try:
-            playback = spotify.playback_currently_playing()
+    try:
+        playback = spotify.playback_currently_playing()
 
-            item = playback.item.name if playback else None
-            page += f'<br>Now playing: {item}'
-        except tk.HTTPError:
-            page += '<br>Error in retrieving now playing!'
+        item = playback.item.name if playback else None
+        page += f'<br>Now playing: {item}'
+    except tk.HTTPError:
+        page += '<br>Error in retrieving now playing!'
 
-        playlists = spotify.playlists(user.id)
-        page += f'<br>Playlists:<ul>'
-        for pl in playlists.items:
-            page += f'<li>{pl.name}</li>'
-        page += '</ul>'
+    playlists = spotify.playlists(user.id)
+    page += f'<br>Playlists:<ul>'
+    for pl in playlists.items:
+        page += f'<li>{pl.name}</li>'
+    page += '</ul>'
 
-        return page
+    return page
 
-    @app.route('/login', methods=['GET'])
-    def login():
-        if 'user' in session:
-            return redirect('/', 307)
-
-        auth = tk.UserAuth(cred, SCOPE)
-        return redirect(auth.url, 307)
-
-    @app.route('/callback', methods=['GET'])
-    def login_callback():
-        code = request.args.get('code', None)
-        state = request.args.get('state', None)
-        # This seems like the wrong way to make this work, but it works.
-        auth = tk.UserAuth(cred, SCOPE)
-        auth.state = state
-
-        if auth is None:
-            return 'Invalid state!', 400
-
-        token = auth.request_token(code, state)
-        session['user'] = state
-        users[state] = token
+@app.route('/login', methods=['GET'])
+def login():
+    if 'user' in session:
         return redirect('/', 307)
 
-    @app.route('/logout', methods=['GET'])
-    def logout():
-        uid = session.pop('user', None)
-        if uid is not None:
-            users.pop(uid, None)
-        return redirect('/', 307)
+    auth = tk.UserAuth(cred, SCOPE)
+    return redirect(auth.url, 307)
 
-    return app
+@app.route('/callback', methods=['GET'])
+def login_callback():
+    code = request.args.get('code', None)
+    state = request.args.get('state', None)
+    # This seems like the wrong way to make this work, but it works.
+    auth = tk.UserAuth(cred, SCOPE)
+    auth.state = state
 
+    if auth is None:
+        return 'Invalid state!', 400
 
-app = app_factory()
+    token = auth.request_token(code, state)
+    session['user'] = state
+    users[state] = token
+    return redirect('/', 307)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    uid = session.pop('user', None)
+    if uid is not None:
+        users.pop(uid, None)
+    return redirect('/', 307)
+
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
